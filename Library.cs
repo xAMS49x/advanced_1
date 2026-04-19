915ded13ba192267855fc180029f8dc2a1def5d2
xAMS49x <ams49game@gmail.com>
2026-04-19 18:38:30 +0300

    retry push: drafted 4th lesson task

---
+++ Library.cs
@@ -0,0 +1,105 @@
+using System;
+using System.Text;
+
+namespace Library
+{
+    public class Library
+    {
+        public static void ShowError(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("An error has occured: " + msg);
+            Console.ResetColor();
+        }
+
+        public static string GetString(string msg)
+        {
+            Console.WriteLine(msg);
+            return Console.ReadLine() ?? throw new InvalidOperationException("Null exception occured.");
+        }
+
+        public static int GetInt(string msg)
+        {
+            return Convert.ToInt32(GetString(msg));
+        }
+
+        public static double GetDouble(string msg)
+        {
+            return Convert.ToDouble(GetString(msg));
+        }
+
+        public static byte GetBytes(string msg)
+        {
+            return Convert.ToByte(GetString(msg));
+        }
+
+        public static bool ValidateStringLength(string text, int minLength, int maxLength)
+        {
+            if (text.Trim().Length < minLength || text.Trim().Length > maxLength)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool ValidateRange(byte value, int min, int max)
+        {
+            if (value < min || value > max)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public static int CoinFlip()
+        {
+            var coinToss = new Random();
+            int roll = coinToss.Next(1, 1001);
+
+            switch (roll)
+            {
+                case <= 498:
+                    return 3;
+
+                case <= 996:
+                    return 1;
+
+                default:
+                    return 10;
+            }
+        }
+        
+        public static StringBuilder log = new StringBuilder();
+
+        public static void Log(string message)
+        {
+            Console.WriteLine(message);
+            log.AppendLine($"<program> {message}");
+        }
+
+        public static string Ask(string prompt)
+        {
+            Console.WriteLine(prompt);
+            log.Append($"<program> {prompt}");
+
+            string input = Console.ReadLine()!;
+            log.AppendLine();
+            log.AppendLine($"<user> {input}");
+
+            return input;
+        }
+        
+        public static void SaveLog(int exitCode = 0)
+        {
+            log.AppendLine($"\nProcess finished with exit code {exitCode}");
+
+            string fileName = $"log_{DateTime.Now:yyyy-MM-dd_HH-mm-ss}.txt";
+            string path = Path.Combine(Directory.GetCurrentDirectory(), fileName);
+
+            File.WriteAllText(path, log.ToString());
+            Console.WriteLine($"\nLog saved to: {path}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using static Library.Library;
+
+namespace Homework3
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Dictionary<int, string> phoneDictionary = new Dictionary<int, string>();
+            int orderNumber = 1;
+            string numberName = "";
+            Log("Welcome! Phonebook v0.1 is running.");
+            Log(
+                "Feature list:\n1. Add contact\n2. Edit contact\n3.Delete contact\n4.Search contacts\n5. Show full contact list");
+            byte phoneBookChoice = Convert.ToByte(Ask("What do you want to do? (1-5)"));
+            if (!ValidateRange(phoneBookChoice, 0, 5))
+            {
+                ShowError("Invalid choice");
+                return;
+            }
+
+            switch (phoneBookChoice)
+
+            {
+                case 1:
+                    try
+                    {
+                        // Add contact function
+                        while (true)
+                        {
+                            string numberAdd = Ask("\nEnter a phone number");
+                            if (numberAdd.Trim().Length != 8)
+                            {
+                                ShowError("Invalid phone number");
+                                return;
+                            }
+
+                            phoneDictionary.Add(orderNumber, numberAdd);
+
+                            numberName = Ask("Enter a name for this number");
+                            if (numberName.Trim().Length == 0)
+                            {
+                                ShowError("Name can't be empty");
+                                return;
+                            }
+
+                            Log("Number added as " + phoneDictionary[orderNumber] + "\n" + numberName);
+                            orderNumber++;
+                            Log("Want to add another number? (y)");
+                            if (Console.ReadKey().KeyChar != 'y')
+                                break;
+                        }
+
+                        Log("\nPhonebook contents:");
+                        foreach (KeyValuePair<int, string> entry in phoneDictionary)
+                        {
+                            Log($": {entry.Key}:{numberName}\n {entry.Value}\n");
+                        }
+
+                        SaveLog(0);
+                    }
+                    catch (Exception exception)
+                    {
+                        Log("An error has occured: " + exception.Message);
+                        SaveLog(1);
+                    }
+
+                    break;
+
+                case 2:
+                
+                    break;
+            }
+        }
+        
+    }
+}
